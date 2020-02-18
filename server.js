9c37c7cf663d8294354f8cdea7792f955d5aa9d0
Raneil <raneil.chavez1@gmail.com>
2020-02-17 16:09:17 -0800

    Adalzxkczlkc

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-// note, io(<port>) will create a http server for you
-var port = process.env.PORT || 3000;
-var io = require('socket.io')(port);
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
-io.on('connection', function (socket) {
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
 	console.log("Connected!");
 
 	socket.on('chat', function(data){
@@ -16,6 +20,8 @@ io.on('connection', function (socket) {
 	socket.on('disconnect', function () {
 		io.emit('user disconnected');
 	});
+});
 
-	
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
